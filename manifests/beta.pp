65df3e4a6b9d342ccb9e014f86c3c182b2ea9594
lonnen <chris.lonnen@gmail.com>
2014-06-10 11:43:56 -0700

    update for Firefox release=30
    also updates cardboard to ~2.0

--- manifests/beta.pp
+++ manifests/beta.pp
@@ -3,7 +3,7 @@
 # Examples
 #
 #   include firefox::beta
-class firefox::beta ($locale = 'en-US', $version = '30.0b7'){
+class firefox::beta ($locale = 'en-US', $version = '31.0b1'){
   package { 'Firefox-Beta':
     source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${version}/mac/${locale}/Firefox%20${version}.dmg",
     provider => 'appdmg'
